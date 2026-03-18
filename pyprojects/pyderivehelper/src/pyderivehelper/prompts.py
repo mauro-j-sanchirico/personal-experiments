739a683e0044daa73588aab3d2a0a08ba464cb94
Mauro J. Sanchirico III <156733573+mauro-j-sanchirico@users.noreply.github.com>
2026-03-18 15:41:49 -0400

    Wolfram Language generation working as library call

--- pyprojects/pyderivehelper/src/pyderivehelper/prompts.py
+++ pyprojects/pyderivehelper/src/pyderivehelper/prompts.py
@@ -1,4 +1,6 @@
-wolfram_code_generator = """
+from dataclasses import dataclass
+
+_WOLFRAM_CODE_GENERATOR_TEXT = """
 You a Wolfram Language code generator.
 
 Your task is to translate natural language descriptions of mathematical operations into executable Wolfram Language code.
@@ -12,3 +14,19 @@ No surrounding text.
 Return exactly one valid Wolfram Language expression.
 Ensure the result can be evaluated directly in a Wolfram kernel.
 """
+
+
+@dataclass
+class SystemPrompts:
+    wolfram_code_generator: str = _WOLFRAM_CODE_GENERATOR_TEXT
+
+
+def populate_wolfram_code_generator_prompt_template(prompt):
+    prompt_template = f"""
+Convert the following description into a Wolfram Language expression.
+Description:
+{prompt}
+Wolfram Language:
+"""
+
+    return prompt_template
--- pyprojects/pyderivehelper/src/pyderivehelper/wltools.py
+++ pyprojects/pyderivehelper/src/pyderivehelper/wltools.py
@@ -3,14 +3,17 @@ import os
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
-from IPython.display import Math, display
+from IPython.display import Markdown, Math, display
 from openai import OpenAI
 from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wl
 
 from pyderivehelper.config_openai import OpenAIModels
 from pyderivehelper.config_wl import _FAILED_RESULT
-from pyderivehelper.system_prompts import wolfram_code_generator
+from pyderivehelper.prompts import (
+    SystemPrompts,
+    populate_wolfram_code_generator_prompt_template,
+)
 
 # Load environment variables
 load_dotenv()
@@ -25,26 +28,43 @@ math_assistant_client = OpenAI(
 
 
 def print_tex(expr):
-    """Prints an expression in latex"""
+    """Prints an raw expression in rendered TeX."""
     display(Math(expr))
 
 
 def print_wexpr(expr):
-    """Prints a wolfram language result in latex"""
+    """Prints a Wolfram Language expression in rendered TeX."""
     tex_expr = ws.evaluate(wl.ToString(wl.TeXForm(expr)))
     display(Math(tex_expr))
 
 
 def print_wresult(expr):
-    """Prints a wolfram language expression in latex"""
+    """Evaluates Wolfram Language expression and renders the result via TeX."""
     tex_expr = ws.evaluate(wl.ToString(wl.TeXForm(ws.evaluate(expr))))
     display(Math(tex_expr))
 
 
 def print_wresult_tex(expr):
-    """Prints a wolfram language expression in latex"""
+    """Prints a Wolfram Language expression in raw TeX form."""
     tex_expr = ws.evaluate(wl.ToString(wl.TeXForm(ws.evaluate(expr))))
     print(tex_expr)
+
+
+def check_syntax(expr):
+    valid_syntax_check_str = f'SyntaxQ["{expr}"]'
+    syntax_check_result = str(ws.evaluate(valid_syntax_check_str))
+    if _FAILED_RESULT in syntax_check_result:
+        return False
+    return True
+
+
+def wplot(filename, command):
+    export_expr = f'Export["{filename}", {command}]'
+    ws.evaluate(export_expr)
+    img = mpimg.imread(filename)
+    plt.imshow(img)
+    plt.show()
+    plt.axis('off')
 
 
 def wc(expr):
@@ -57,19 +77,37 @@ def wc(expr):
 
 
 def wnlp(prompt, model=OpenAIModels.mini):
-    prompt_template = f"""
-Convert the following description into a Wolfram Language expression.
+    # Generate code
+    print('Generating Wolfram Language code...')
+    response_str = evaluate_wolfram_language_from_prompt(model, prompt)
+    display(Markdown('\n**Wolfram Code**:\n'))
+    display(Markdown(f'```wolfram\n{response_str}\n```'))
+    # TODO: Check if the response is valid Wolfram Language code
 
-Description:
-{prompt}
+    # TODO: Check if there is a plot and call plot if there is
 
-Wolfram Language:
-"""
+    # Evaluate the result and display in rendered TeX
+    display(Markdown('\n**Evaluated Result**:\n'))
+    result = wc(response_str)
+
+    # Render raw TeX for copy-pasting
+    display(Markdown('\n**Raw TeX**:\n'))
+    tex_str = print_wresult_tex(result)
+    display(Markdown(f'```tex\n{tex_str}\n```'))
+
+    return result
+
+
+def evaluate_wolfram_language_from_prompt(model, prompt):
+    prompt_template = populate_wolfram_code_generator_prompt_template(prompt)
     response = math_assistant_client.responses.create(
         model=model,
         temperature=0,
         input=[
-            {'role': 'system', 'content': wolfram_code_generator},
+            {
+                'role': 'system',
+                'content': SystemPrompts.wolfram_code_generator,
+            },
             {
                 'role': 'user',
                 'content': prompt_template,
@@ -77,22 +115,4 @@ Wolfram Language:
         ],
     )
     response_str: str = response.output_text.strip()
-
     return response_str
-
-
-def wplot(filename, command):
-    export_expr = f'Export["{filename}", {command}]'
-    ws.evaluate(export_expr)
-    img = mpimg.imread(filename)
-    plt.imshow(img)
-    plt.show()
-    plt.axis('off')
-
-
-def check_syntax(expr):
-    valid_syntax_check_str = f'SyntaxQ["{expr}"]'
-    syntax_check_result = str(ws.evaluate(valid_syntax_check_str))
-    if syntax_check_result == _FAILED_RESULT:
-        return False
-    return True
